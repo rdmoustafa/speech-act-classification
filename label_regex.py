a6b01dca1ec8ec0bb7c257b124e6c305b46832fd
reem <reemdiaa@gmail.com>
2024-02-20 20:44:04 +0000

    Split the regular expression for situation to be smaller categorise

--- label_regex.py
+++ label_regex.py
@@ -1,32 +1,52 @@
 import pandas as pd
 import re  # regular expressions
 
+# TODO split situation awareness into smaller groups
+# Define your regular expressions and corresponding labels
+original_patterns = {
+    'Communications': r'alpha|bravo|charlie|allcallsigns|roger|over|\^cop\$',
+    'Intel (from newspapers)': r'squirrel|steel|conference|soccer|music|football|\^relig\$|\^advert\$|stolen'
+                               r'|arrest|festival|family|cottage|interfaith|honda|theft|royal|newspaper|community'
+                               r'|princess|visit|\^canad',
+    'Situation Awareness': r'\^north\$|\^south\$|\^east\$|\^west\$|\^locat\$|hospital|\^camp\$|police|building'
+                           r'|wind|draysend|charville|firwood|greenhill|wychewood|woodside|sunwood|westhill|the '
+                           r'copse|esterly|newforest|oldtown|lowtown|newton|black '
+                           r'hill|dripshill|malton|hollywood|winterfold|shrawleywood|beaconhill|trenchwood'
+                           r'|casltehill|linkwood|underwood|wyreforest|castleton|wildwood|hanley|swanton'
+                           r'|holbeechwood|astley|thetfordwood|holvern|langdalewood|thetford|brightwood|epping'
+                           r'|worthycopse|breydon|denston|worthington',
+    'Fire words': r'\^fire\$|water|replen|\^fill\$|\^burn\$|\^extinguish\$|bowser',
+    'Rescue words': r'\^load\$|pax|\^evac\$|\^person\$|\^rescue\$|people',
+    'Action words': r'recce|check|\^mov\$|\^send\$|support|try|find|support|\^go\$',
+    'Reasoning words': r'suggest|\^belie\$|looks|ignore|think|argue'
+}
+
+patterns = {
+    # "Communications": r"alpha|bravo|charlie|allcallsigns|roger|over|\^cop\$",
+    "Intel (from newspapers)": r"squirrel|steel|conference|soccer|music|football|\^relig\$|\^advert\$|stolen"
+                               r"|arrest|festival|family|cottage|interfaith|honda|theft|royal|newspaper|community"
+                               r"|princess|visit|\^canad",
+    "Directions": r'\^north|\^south|\^east|\^west|\blocat\w*\b',
+    "Woods": r"firwood|wychewood|woodside|shrawleywood|holbeechwood|hollywood|sunwood|trenchwood|wildwood|brightwood|linkwood|underwood|langdalewood|thetfordwood",
+    "Buildings": r"hospital|\^camp\$|police|building|wind",
+    "Hills and Forests": r"beaconhill|casltehill|greenhill|westhill|black hill|dripshill|newforest|wyreforest",
+    "Named Locations": "draysend|charville|the copse|esterly|oldtown|lowtown|newtonmalton|winterfold|castleton|hanley|swanton|astley|holvern|thetford|epping|worthycopse|breydon|denston|worthington",
+    "Fire words": r"\^fire\$|water|replen|\^fill\$|\^burn\$|\^extinguish\$|bowser",
+    "Rescue words": r"\^load\$|pax|\^evac\$|\^person\$|\^rescue\$|people",
+    "Action words": r"recce|check|\^mov\$|\^send\$|support|try|find|support|\^go\$",
+    "Reasoning words": r"suggest|\^belie\$|looks|ignore|think|argue"
+}
+
+unique_labels = list(patterns.keys())
+print(unique_labels)
+
+# Define a regular expression pattern to split the paragraph into sentences
+sentence_pattern = r'(?<=[.!?])\s+'
+
 
 def single_label(path):
     # Load the CSV file into a pandas DataFrame
     raw_df = pd.read_csv(path)
-
-    # Define your regular expressions and corresponding labels
-    patterns = {
-        'Communications': r'alpha|bravo|charlie|allcallsigns|roger|over|\^cop\$',
-        'Intel (from newspapers)': r'squirrel|steel|conference|soccer|music|football|\^relig\$|\^advert\$|stolen'
-                                   r'|arrest|festival|family|cottage|interfaith|honda|theft|royal|newspaper|community'
-                                   r'|princess|visit|\^canad',
-        'Situation Awareness': r'\^north\$|\^south\$|\^east\$|\^west\$|\^locat\$|hospital|\^camp\$|police|building'
-                               r'|wind|draysend|charville|firwood|greenhill|wychewood|woodside|sunwood|westhill|the '
-                               r'copse|esterly|newforest|oldtown|lowtown|newton|black '
-                               r'hill|dripshill|malton|hollywood|winterfold|shrawleywood|beaconhill|trenchwood'
-                               r'|casltehill|linkwood|underwood|wyreforest|castleton|wildwood|hanley|swanton'
-                               r'|holbeechwood|astley|thetfordwood|holvern|langdalewood|thetford|brightwood|epping'
-                               r'|worthycopse|breydon|denston|worthington',
-        'Fire words': r'\^fire\$|water|replen|\^fill\$|\^burn\$|\^extinguish\$|bowser',
-        'Rescue words': r'\^load\$|pax|\^evac\$|\^person\$|\^rescue\$|people',
-        'Action words': r'recce|check|\^mov\$|\^send\$|support|try|find|support|\^go\$',
-        'Reasoning words': r'suggest|\^belie\$|looks|ignore|think|argue'
-    }
-
-    # Define a regular expression pattern to split the paragraph into sentences
-    sentence_pattern = r'(?<=[.!?])\s+'
 
     # Split the paragraph into sentences and clean up the unnecessary dots and make it lower case
     raw_sentences = []
@@ -75,35 +95,10 @@ def multiclass_labelling(path):
     # Load the CSV file into a pandas DataFrame
     raw_df = pd.read_csv(path)
 
-    # Define your regular expressions and corresponding labels
-    patterns = {
-        'Communications': r'alpha|bravo|charlie|allcallsigns|roger|over|\^cop\$',
-        'Intel (from newspapers)': r'squirrel|steel|conference|soccer|music|football|\^relig\$|\^advert\$|stolen'
-                                   r'|arrest|festival|family|cottage|interfaith|honda|theft|royal|newspaper|community'
-                                   r'|princess|visit|\^canad',
-        'Situation Awareness': r'\^north\$|\^south\$|\^east\$|\^west\$|\^locat\$|hospital|\^camp\$|police|building'
-                               r'|wind|draysend|charville|firwood|greenhill|wychewood|woodside|sunwood|westhill|the '
-                               r'copse|esterly|newforest|oldtown|lowtown|newton|black '
-                               r'hill|dripshill|malton|hollywood|winterfold|shrawleywood|beaconhill|trenchwood'
-                               r'|casltehill|linkwood|underwood|wyreforest|castleton|wildwood|hanley|swanton'
-                               r'|holbeechwood|astley|thetfordwood|holvern|langdalewood|thetford|brightwood|epping'
-                               r'|worthycopse|breydon|denston|worthington',
-        'Fire words': r'\^fire\$|water|replen|\^fill\$|\^burn\$|\^extinguish\$|bowser',
-        'Rescue words': r'\^load\$|pax|\^evac\$|\^person\$|\^rescue\$|people',
-        'Action words': r'recce|check|\^mov\$|\^send\$|support|try|find|support|\^go\$',
-        'Reasoning words': r'suggest|\^belie\$|looks|ignore|think|argue'
-    }
-
     data_dict = {'Sentence': []}
-
-    unique_labels = ["Communications", "Intel (from newspapers)", "Situation Awareness", "Fire words", "Rescue words",
-                     "Action words", "Reasoning words"]
 
     for label in unique_labels:
         data_dict[label] = []
-
-    # Define a regular expression pattern to split the paragraph into sentences
-    sentence_pattern = r'(?<=[.!?])\s+'
 
     # Split the paragraph into sentences and clean up the unnecessary dots and make it lower case
     raw_sentences = []
@@ -117,7 +112,7 @@ def multiclass_labelling(path):
             continue
         clean_sentences.append(sentence.replace(".", "").strip())
 
-    print(clean_sentences)
+    # print(clean_sentences)
 
     # For each sentence, go through the regex and add label them for each one they match to
     labeled_sentences = []
@@ -130,7 +125,8 @@ def multiclass_labelling(path):
         labeled_sentences.append((sentence, matched_labels))
 
     for sentence, matched_labels in labeled_sentences:
-        print(sentence, matched_labels)
+        if "Locations" in matched_labels:
+            print(sentence, matched_labels)
 
         data_dict['Sentence'].append(sentence)
         for label in unique_labels:
@@ -142,8 +138,11 @@ def multiclass_labelling(path):
         # Convert dictionary to DataFrame
     df = pd.DataFrame(data_dict)
 
+    excel_file = "data/labeled_multiclass.xlsx"
     # Save DataFrame to Excel file
-    df.to_excel("data/labeled_multiclass.xlsx", index=False)
+    df.to_excel(excel_file, index=False)
+
+    print("Data has been written to ", excel_file)
 
 
 if __name__ == "__main__":
