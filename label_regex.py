626804d64b3e431bf36ace69f40356f3f0c72d9c
reem <reemdiaa@gmail.com>
2024-03-23 17:05:06 +0000

    Implemented single label priority method

--- label_regex.py
+++ label_regex.py
@@ -146,6 +146,76 @@ def multiclass_labelling(path):
     print("Data has been written to ", excel_file)
 
 
+def single_label_priority(path):
+    # Load the CSV file into a pandas DataFrame
+    raw_df = pd.read_csv(path)
+
+    # Split the paragraph into sentences and clean up the unnecessary dots and make it lower case
+    raw_sentences = []
+    for row in raw_df["Text"]:
+        raw_sentences += (re.split(sentence_pattern.lower(), row.lower()))
+
+    clean_sentences = []
+
+    for index, sentence in enumerate(raw_sentences):
+        if sentence.strip() == "." or sentence.strip() == "":
+            continue
+        clean_sentences.append(sentence.replace(".", "").strip())
+
+    # For each sentence, go through the regex and add label them for each one they match to
+    labeled_sentences = []
+
+    ordered_patterns = {
+        "Rescue words": r"\^load\$|pax|\^evac\$|\^person\$|\^rescue\$|people",
+        "Action words": r"recce|check|\^mov\$|\^send\$|support|try|find|support|\^go\$",
+        "Reasoning words": r"suggest|\^belie\$|looks|ignore|think|argue",
+        "Directions": r'\^north|\^south|\^east|\^west|\blocat\w*\b',
+        "Fire words": r"fire|water|replen|\^fill\$|\^burn\$|\^extinguish\$|bowser",
+        "Woods": r"firwood|wychewood|woodside|shrawleywood|holbeechwood|hollywood|sunwood|trenchwood|wildwood|brightwood"
+                 r"|linkwood|underwood|langdalewood|thetfordwood",
+        "Buildings": r"hospital|\^camp\$|police|building|wind",
+        "Hills and Forests": r"beaconhill|casltehill|greenhill|westhill|black hill|dripshill|newforest|wyreforest",
+        "Intel (from newspapers)": r"squirrel|steel|conference|soccer|music|football|\^relig\$|\^advert\$|stolen"
+                                   r"|arrest|festival|family|cottage|interfaith|honda|theft|royal|newspaper|community"
+                                   r"|princess|visit|\^canad",
+        "Named Locations": "draysend|charville|the copse|esterly|oldtown|lowtown|newtonmalton|winterfold|castleton|hanley"
+                           "|swanton|astley|holvern|thetford|epping|worthycopse|breydon|denston|worthington",
+        "Communications": r"alpha|bravo|charlie|allcallsigns|roger|over|\^cop\$|copy",
+    }
+
+    for sentence in clean_sentences:
+        matched_labels = []
+        for label_name, pattern in ordered_patterns.items():
+            # Priority system for the patterns?
+            if re.search(pattern, sentence):
+                matched_labels.append(label_name)
+                break
+
+        if len(matched_labels) == 0:
+            labeled_sentences.append((sentence, "Other"))
+        elif len(matched_labels) == 1:
+            labeled_sentences.append((sentence, matched_labels[0]))
+        else:
+            print("what the fuck")
+        # else:
+        #     for match in matched_labels:
+        #         if match != "Communications":
+        #             labeled_sentences.append((sentence, match))
+
+    # This will make the data set unique, will not repeat the same tuple
+    labeled_df = pd.DataFrame(list(set(labeled_sentences)), columns=['Sentence', 'Label'])
+    # labeled_df = pd.DataFrame(labeled_sentences, columns=['Sentence', 'Labels'])
+
+    csv_file = "data/labeled_sentences_prioritised.csv"
+    # Save the DataFrame to a CSV file
+    labeled_df.to_csv(csv_file, index=False)
+
+    print("Data has been written to: ", csv_file)
+    # return labeled_sentences
+
+
 if __name__ == "__main__":
-    single_label("data/full-raw-transcript.csv")
-    multiclass_labelling("data/full-raw-transcript.csv")
+    # single_label("data/full-raw-transcript.csv")
+    # multiclass_labelling("data/full-raw-transcript.csv")
+
+    single_label_priority("data/full-raw-transcript.csv")
